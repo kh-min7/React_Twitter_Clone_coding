7b54090407d91f55fa64fcdaf5a7c10e29b6abc9
kh-min7 <rudgh2879@naver.com>
2022-10-06 21:28:10 +0900

    2.4 Log In

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,26 @@
-import { useState } from "react";
 import AppRouter from "components/Router";
+import { useState, useEffect } from "react";
 import { authService } from "fbase";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
-  return <AppRouter isLoggedIn={isLoggedIn} />;
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsLoggedIn(true);
+        const uid = user.uid;
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
+  return (
+    <>{init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Initializing..."}</>
+  );
 }
 
 export default App;
